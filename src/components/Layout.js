32cf72815dbfc6965491600bde7b63c8bb41777a
Nick Gray <nicholasnbg@gmail.com>
2019-01-14 22:47:19 +1100

    Created about page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -32,23 +32,42 @@ class Layout extends React.Component {
       )
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
+        <React.Fragment>
+          <h3
             style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
+              fontFamily: `Montserrat, sans-serif`,
+              marginTop: 0,
             }}
-            to={`/`}
           >
-            {title}
-          </Link>
-        </h3>
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/`}
+            >
+              {title}
+            </Link>
+          </h3>
+          <h6
+            style={{
+              fontFamily: `Montserrat, sans-serif`,
+              marginTop: 0,
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`,
+              }}
+              to={`/about`}
+            >
+              {`About`}
+            </Link>
+          </h6>
+        </React.Fragment>
       )
     }
     return (
---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+class About extends Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        About page
+      </Layout>
+    )
+  }
+}
+
+export default About
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
