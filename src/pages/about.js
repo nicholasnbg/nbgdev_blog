c78ed19fcdc9b3b23e7e070b7c1b1eb029dc4513
nbg <nicholasnbg@gmail.com>
2019-01-17 13:44:59 +1100

    Begin adding content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link, graphql } from 'gatsby'
 import SEO from '../components/seo'
+import styled from 'styled-components'
+
 
 
 import Layout from '../components/Layout'
@@ -16,7 +18,24 @@ class About extends Component {
           title="About Me"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        About page
+        <h1>Hi, I'm Nick</h1>
+        <h2>The Short...</h2>
+        <p>I'm a software engineer at {`{...insert awesome company here}`}.</p>
+        <p>I'm a self taught full stack developer, based in Melbourne, Australia.</p>
+        <p>I love all things programming, but have a passion for full stack Javscript (Node + React), as well as Ruby + Rails </p>
+
+        <h2>Nick the... <em>Developer</em></h2>
+        <p>dafdgaa</p>
+
+        <h2>Nick the ...</h2>
+        <ul>
+          <li>
+            <BulletHeading>Traveller</BulletHeading>
+            <p>I love to travel</p>
+          </li>
+        </ul>
+
+
       </Layout>
     )
   }
@@ -32,4 +51,10 @@ export const pageQuery = graphql`
       }
     }
   }
+`
+
+
+const BulletHeading = styled.span`
+  font-size: 1.3rem;
+  font-weight: 500;
 `
